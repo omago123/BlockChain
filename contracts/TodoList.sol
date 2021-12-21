e2fd750934c889b7c0b36f541cb6e5d5075e68eb
unknown <omago123@naver.com>
2021-12-22 01:31:56 +0900

    app.js

---
+++ contracts/TodoList.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    // index : task
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+}
