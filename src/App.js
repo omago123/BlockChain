9cf5c6372d50066e3e08e7f1976146bbe50ca752
unknown <omago123@naver.com>
2022-01-20 03:14:28 +0900

    UI function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component} from 'react';
 
 import Web3  from 'web3';
 
-let lotteryAddress =   '0x60574b00f370a801BAECE2f965848A087208032e'
+let lotteryAddress =   '0x971719be3e765C16BE98fb18d3570AeD099403E2'
 let lotteryAbi = [ { "constant": true, "inputs": [], "name": "answerForTest", "outputs": [ { "name": "", "type": "bytes32" } ], "payable": false, "stateMutability": "view", "type": "function", "signature": "0x84f7e4f0" }, { "constant": true, "inputs": [], "name": "owner", "outputs": [ { "name": "", "type": "address" } ], "payable": false, "stateMutability": "view", "type": "function", "signature": "0x8da5cb5b" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor", "signature": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "index", "type": "uint256" }, { "indexed": false, "name": "bettor", "type": "address" }, { "indexed": false, "name": "amount", "type": "uint256" }, { "indexed": false, "name": "challenges", "type": "bytes1" }, { "indexed": false, "name": "answerBlockNumber", "type": "uint256" } ], "name": "BET", "type": "event", "signature": "0x100791de9f40bf2d56ffa6dc5597d2fd0b2703ea70bc7548cd74c04f5d215ab7" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "index", "type": "uint256" }, { "indexed": false, "name": "bettor", "type": "address" }, { "indexed": false, "name": "amount", "type": "uint256" }, { "indexed": false, "name": "challenges", "type": "bytes1" }, { "indexed": false, "name": "answer", "type": "bytes1" }, { "indexed": false, "name": "answerBlockNumber", "type": "uint256" } ], "name": "WIN", "type": "event", "signature": "0x8219079e2d6c1192fb0ff7f78e6faaf5528ad6687e69749205d87bd4b156912b" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "index", "type": "uint256" }, { "indexed": false, "name": "bettor", "type": "address" }, { "indexed": false, "name": "amount", "type": "uint256" }, { "indexed": false, "name": "challenges", "type": "bytes1" }, { "indexed": false, "name": "answer", "type": "bytes1" }, { "indexed": false, "name": "answerBlockNumber", "type": "uint256" } ], "name": "DRAW", "type": "event", "signature": "0x72ec2e949e4fad9380f9d5db3e2ed0e71cf22c51d8d66424508bdc761a3f4b0e" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "index", "type": "uint256" }, { "indexed": false, "name": "bettor", "type": "address" }, { "indexed": false, "name": "amount", "type": "uint256" }, { "indexed": false, "name": "challenges", "type": "bytes1" }, { "indexed": false, "name": "answer", "type": "bytes1" }, { "indexed": false, "name": "answerBlockNumber", "type": "uint256" } ], "name": "FAIL", "type": "event", "signature": "0x3b19d607433249d2ebc766ae82ca3848e9c064f1febb5147bc6e5b21d0adebc5" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "index", "type": "uint256" }, { "indexed": false, "name": "bettor", "type": "address" }, { "indexed": false, "name": "amount", "type": "uint256" }, { "indexed": false, "name": "challenges", "type": "bytes1" }, { "indexed": false, "name": "answerBlockNumber", "type": "uint256" } ], "name": "REFUND", "type": "event", "signature": "0x59c0185881271a0f53d43e6ab9310091408f9e0ff9ae2512613de800f26b8de4" }, { "constant": true, "inputs": [], "name": "getPot", "outputs": [ { "name": "pot", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function", "signature": "0x403c9fa8" }, { "constant": false, "inputs": [ { "name": "challenges", "type": "bytes1" } ], "name": "betAndDistribute", "outputs": [ { "name": "result", "type": "bool" } ], "payable": true, "stateMutability": "payable", "type": "function", "signature": "0xe16ea857" }, { "constant": false, "inputs": [ { "name": "challenges", "type": "bytes1" } ], "name": "bet", "outputs": [ { "name": "result", "type": "bool" } ], "payable": true, "stateMutability": "payable", "type": "function", "signature": "0xf4b46f5b" }, { "constant": true, "inputs": [ { "name": "challenges", "type": "bytes1" }, { "name": "answer", "type": "bytes32" } ], "name": "isMatch", "outputs": [ { "name": "", "type": "uint8" } ], "payable": false, "stateMutability": "pure", "type": "function", "signature": "0x99a167d7" }, { "constant": false, "inputs": [ { "name": "answer", "type": "bytes32" } ], "name": "setAnswerForTest", "outputs": [ { "name": "result", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function", "signature": "0x7009fa36" }, { "constant": false, "inputs": [], "name": "distribute", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function", "signature": "0xe4fc6b6d" }, { "constant": true, "inputs": [ { "name": "index", "type": "uint256" } ], "name": "getBetInfo", "outputs": [ { "name": "answerBlockNumber", "type": "uint256" }, { "name": "bettor", "type": "address" }, { "name": "challenges", "type": "bytes1" } ], "payable": false, "stateMutability": "view", "type": "function", "signature": "0x79141f80" } ]
 
 class App extends Component{
@@ -30,9 +30,16 @@ class App extends Component{
 
   async componentDidMount() {
     await this.initWeb3();
-    await this.getBetEvents();
+    await this.pollData();
+    // setInterval(this.pollData, 1000);
+    
   }
 
+  pollData = async () => {
+    await this.getPot();
+    await this.getBetEvents();
+    await this.getWinEvents();
+  }
   initWeb3 = async () => {
     // Modern dapp browers...
     if (window.ethereum){
@@ -65,24 +72,64 @@ class App extends Component{
     this.lotteryContract = new this.web3.eth.Contract(lotteryAbi, lotteryAddress);
 
     // smart contract 값을 변화시키지 않는 값을 불러오는 함수를 호출할 때 call을 사용
-    // let pot = this.lotteryContract.methods.getPot().call()
-    // console.log(pot)
-
-    // let owner = this.lotteryContract.methods.owner().call()
-    // console.log(owner)
-
+    //  let pot = this.lotteryContract.methods.getPot().call(); 
     
+    //  console.log(pot)
+
+    //  //let bettor='0x84F43c88Dc56510D043D5EE922D57549959c4C3C'
+    //  let owner = this.lotteryContract.methods.owner().call()
+    //  console.log(owner)
+     
+     
+
+     
+  }
+
+  getPot = async () => {
+    let pot = await this.lotteryContract.methods.getPot().call();
+    let potString = this.web3.utils.fromWei(pot.toString(), 'ether');
+    this.setState({pot:potString})
   }
 
   getBetEvents = async () => {
+    const records = [];
     let events = await this.lotteryContract.getPastEvents('BET', {fromBlock:0, toBlock:'latest'});
-    console.log(events);
+    for(let i=0; i<events.length; i+=1){
+      const record = {}
+      record.index = parseInt(events[i].returnValues.index, 10).toString();
+      record.bettor = events[i].returnValues.bettor;
+      record.betBlockNumber = events[i].blockNumber;
+      record.targetBlockNumber = events[i].returnValues.answerBlockNumber.toString();
+      record.challenges = events[i].returnValues.challenges;
+      record.win = 'Not Revealed';
+      record.answer ='0x00';
+      records.unshift(record);
+    }
+    console.log(records)
+    this.setState({betRecords:records})
+  }
+
+  getWinEvents = async () => {
+    const records = [];
+    let events = await this.lotteryContract.getPastEvents('WIN', {fromBlock:0, toBlock:'latest'});
+    for(let i=0; i<events.length; i+=1){
+      const record = {}
+      record.index = parseInt(events[i].returnValues.index, 10).toString();
+      record.amount = parseInt(events[i].returnValues.amount, 10).toString();
+      records.unshift(record);
+    }
+    console.log(records)
+    this.setState({winRecords:records})
   }
   
   bet = async () => {
     // nonce
+    let challenges = '0x' + this.state.challenges[0].toLowerCase() + this.state.challenges[1].toLowerCase();
     let nonce = await this.web3.eth.getTransaction(this.account);
-    this.lotteryContract.methods.betAndDistribute('0xcd').send({from:this.account, value:5000000000000000, gas:300000,nonce:nonce})
+    this.lotteryContract.methods.betAndDistribute(challenges).send({from:this.account, value:5000000000000000, gas:300000,nonce:nonce})
+    // .on('transactionHash',(hash) =>{
+    //   console.log(hash)
+    // })
   }
 
   // Pot money
@@ -92,6 +139,12 @@ class App extends Component{
 
   // History table
   // index address challenge answer pot status answerBlockNumber
+
+  onClickCard = (_Character) => {
+    this.setState({
+      challenges : [this.state.challenges[1], _Character]
+    })
+  }
 
   getCard = (_Character, _cardStyle) => {
     let _card = '';
@@ -104,12 +157,14 @@ class App extends Component{
     if(_Character === 'C'){
       _card= '🃁'
     }
-    if(_Character === 'D'){
+    if(_Character === '0'){
       _card = '🃑' 
     }
 
     return (
-      <button className={_cardStyle}>
+      <button className={_cardStyle} onClick = {() => { 
+        this.onClickCard(_Character)
+      }}>
           <div className="card-body text-center">
           <p className="card-text"></p>
           <p className="card-text text-center" style={{fontSize:300}}>{_card}</p>
@@ -140,12 +195,12 @@ class App extends Component{
           {this.getCard('A', 'card bg-primary')}
           {this.getCard('B', 'card bg-warning')}
           {this.getCard('C', 'card bg-danger')}
-          {this.getCard('D', 'card bg-success')}
+          {this.getCard('0', 'card bg-success')}
         </div> 
       </div>
     <br></br>
     <div className="container">
-      <button className="btn btn-danger btn-lg">BET!</button>
+      <button className="btn btn-danger btn-lg" onClick={this.bet} >BET!</button>
     </div>
     <br></br>
     <div className="container">
